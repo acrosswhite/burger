e9460d1883f9451ab81a863c4887f4b29363e689
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-11-23 14:26:20 -0800

    update with seed data

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+/* Create and use the burger db */
+CREATE DATABASE  `burger`;
+USE `burger`;
+
+/* Create a table for all burgers */
+CREATE TABLE `allburgers` (
+	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+	`burger_name` VARCHAR( 255) NOT NULL,
+	`devoured` BOOLEAN NOT NULL,
+	`date` TIMESTAMP,
+
+
+	PRIMARY KEY ( `id` ) ); /* Set ID as primary key */
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Inserted a set of records into the table
+INSERT INTO allburgers (burger_name, devoured, date) VALUES ("Veggie Burger", TRUE, 11/23/17);
+INSERT INTO allburgers (burger_name, devoured, date) VALUES ("Bacon Burger", TRUE, 11/23/17);
+INSERT INTO allburgers (burger_name, devoured, date) VALUES ("Double Down Burger", TRUE, 11/23/17);
+INSERT INTO allburgers (burger_name, devoured, date) VALUES ("Cheese Burger", TRUE, 11/23/17);
+
